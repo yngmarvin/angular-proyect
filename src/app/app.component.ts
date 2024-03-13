ac2e28115217d987633e78de73b88787d8e265b9
yngmarvin <marvinbeatss@gmail.com>
2024-03-13 13:39:10 +0100

    calculadora angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,42 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-proyect';
+
+name: string = "Jorge";
+age: number = 32;
+friends: Array<string> = ["2", "3"];
+random: Array<any> = ["Hola", true, 25];
+  
+resultado: number|string = '';
+data1: number = 0;
+data2: number = 0;
+
+public setData1(event :any){
+  this.data1 = Number(event.target.value)
+}
+public setData2(event :any){
+  this.data2 = Number(event.target.value)
+}
+
+
+
+
+public calculator(number1: number,number2: number,operation:string): number | any {
+  switch (operation) {
+    case '+':
+      return this.resultado = number1 + number2;
+    case "-":
+      return this.resultado = number1 + number2;
+    case "*":
+      return this.resultado = number1 * number2;
+    case "/":
+      return this.resultado = number1 / number2;
+
+    
+    default:
+      this.resultado = "operación incorrecta"
+      break;
+  }
+}
+
 }
